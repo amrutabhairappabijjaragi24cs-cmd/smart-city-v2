acd243b537cedaeb7fa09fc5295f27771942d05e
AMRUTA BHAIRAPPA BIJJARAGI <amrutabhairappabijjaragi24cs@rnsit.ac.in>
2026-05-10 15:31:30 +0530

    Fixed API URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://smartcity-backend-xsxs.onrender.com',
+  baseURL: 'https://smartcity-backend-xsxs.onrender.com/api',
   timeout: 10000,
 });
 
